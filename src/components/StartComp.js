64060d683bff7179bfb60237815d181532b20a22
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-03-29 22:36:58 +0530

    Add the starting comp

---
+++ src/components/StartComp.js
@@ -0,0 +1,16 @@
+// Starting Component
+function StartComp({startQuiz}) {
+    return (
+        <div className="comp-card">
+            <div className="card-body">
+                <div className="card-text">
+                    <h1>Begin the Tech Quiz!</h1>
+                    <p>We wil wish you luck!</p>
+                    <button className="btn btn-start" onClick={startQuiz}>Start</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StartComp
